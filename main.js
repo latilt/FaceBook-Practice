9eccf76c859b5fe35f17a09cb8ece30082651fc6
HoKyeong Eom <latilt2@gmail.com>
2017-05-31 11:25:47 +0900

    [MODIFY] main.js

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ whatEvent.addEventListener("focus", function() {
   var ulNode = document.querySelector("#middle-ul ul");
   var cancelNode = document.querySelector("#cancel");
 
-  ulNode.style.display = "none";
-  cancelNode.style.display = "block";
+  /*ulNode.style.display = "none";
+  cancelNode.style.display = "block";*/
+  ulNode.classList.toggle("none");
+  cancelNode.classList.toggle("none");
 });
 
 whatEvent.addEventListener("focusout", function() {
   var ulNode = document.querySelector("#middle-ul ul");
   var cancelNode = document.querySelector("#cancel");
 
-  ulNode.style.display = "block";
-  cancelNode.style.display = "none";
+  /*ulNode.style.display = "block";
+  cancelNode.style.display = "none";*/
+  ulNode.classList.toggle("none");
+  cancelNode.classList.toggle("none");
 });
 
 whatEvent.addEventListener("input", function(evt) {
@@ -23,13 +27,14 @@ whatEvent.addEventListener("input", function(evt) {
   else {
     evt.target.nextElementSibling.style.display = "none";
   }
-
 });
 
 
 var cancelEvent = document.querySelector("#cancel");
 
 cancelEvent.addEventListener("click", function(evt) {
-  evt.target.style.display = "none";
-  evt.target.previousElementSibling.style.display = "block";
+  /*evt.target.style.display = "none";
+  evt.target.previousElementSibling.style.display = "block";*/
+  evt.target.classList.toggle("none");
+  evt.target.previousElementSibling.classList.toggle("none");
 });
